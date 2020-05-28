ef3ca03b16a39f23fd9441624d43131147c33956
meinu-chan <62944053+meinu-chan@users.noreply.github.com>
2020-05-28 15:33:36 +0300

    v2

---
+++ M2LenchenkoV4/BossComp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace M2LenchenkoV4
+{
+    class BossComp:MainComp
+    {
+        protected List<MainComp> childs { get; private set; }
+        private float allSale;
+
+        public BossComp(int alls, int subdistnum)
+        {
+            this.childs = new List<MainComp>();
+            AddChild(this.childs, subdistnum);
+            this.allSale = alls * 100;
+        }
+
+        public override float GetTotal()
+        {
+            return this.allSale += ChildTotal();
+        }
+
+        public override void Add(MainComp component)
+        {
+            Add(component);
+        }
+
+        private float ChildTotal()
+        {
+            float sum = 0;
+            foreach (var component in this.childs)
+            {
+                sum += component.GetTotal();
+            }
+            return sum;
+        }
+
+        private void AddChild(List<MainComp> mc, int index)
+        {
+            for (int i = 0; i < index; i++)
+            {
+                mc.Add(new SubLeaf());
+            }
+
+        }
+    }
+}
---
+++ M2LenchenkoV4/SubLeaf.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace M2LenchenkoV4
+{
+    class SubLeaf : MainComp
+    {
+        private float _sale { get; set; }
+        private int _salenum 
+        {
+            get => GetSaleNum();
+            set => GetSaleNum();                   
+        }
+
+        private int GetSaleNum()
+        {
+            Console.Write("Enter number for children's sales: ");
+            int n = int.Parse(Console.ReadLine());
+            if (n <= 0)
+                throw new Exception("Invalid number(SubLeaf19)");
+            return n;
+        }
+
+        public SubLeaf()
+        {
+            this._sale += 100 * this._salenum;
+            MoreChildren();
+        }
+
+        private void MoreChildren()
+        {
+            Console.WriteLine("Want some more distribution?");
+            switch(Console.ReadLine())
+            {
+                case "y":
+                    CreateChild();
+                    break;
+                case "n":
+                    GetTotal();
+                    break;
+                default:
+                    throw new Exception("Only 'y' or 'n'.");
+            }
+        }
+
+        private void CreateChild()
+        {
+            float sum = 0;
+            var sb = new SubLeaf();
+            sum += sb.GetTotal();
+            this._sale += sum;
+        }
+
+        public override float GetTotal()
+        {
+            return this._sale * .05f;
+        }
+    }
+}
